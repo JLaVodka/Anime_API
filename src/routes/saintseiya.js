8326ed2a23c68e1b709b993a04423866df294394
Andres Delgadillo B. <129786979+JLaVodka@users.noreply.github.com>
2026-05-22 21:01:19 -0500

    Add files via upload

---
+++ src/routes/saintseiya.js
@@ -0,0 +1,58 @@
+const express = require('express');
+
+const router = express.Router();
+
+const db =
+  require('../db/connection');
+
+/**
+ * @swagger
+ * /api/saintseiya/{nombre}:
+ *   get:
+ *     summary:
+ *       Obtener un personaje de Saint Seiya
+ */
+
+router.get('/', async (req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM saintseiya');
+    res.json(result.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Error en base de datos' });
+  }
+});
+
+router.get('/:nombre', async (req, res) => {
+  const nombre =
+    req.params.nombre.toLowerCase();
+  try {
+    const result = await db.query(
+      'SELECT * FROM saintseiya WHERE nombre = $1',
+      [nombre]
+    );
+
+    if (result.rows.length === 0) {
+
+      return res.status(404).json({
+
+        error:
+          'Personaje no encontrado'
+      });
+    }
+
+    res.json(result.rows[0]);
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+
+      error:
+        'Error en base de datos'
+    });
+  }
+});
+
+module.exports = router;
